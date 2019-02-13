e828caba3e2c54a38619be279ef76548834dec6c
mimipeshy <perisndanu@gmail.com>
2019-02-13 08:32:17 +0300

    [chore #163865217] implement feedback

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,18 +1,24 @@
-from urllib.parse import urlparse
 from app.api.responses import Responses
-from flask import jsonify
 
 
 class Validations:
 
     @staticmethod
-    def verify_political_details(name,hqAddress, logoUrl):
+    def verify_political_details(name, hqAddress, logoUrl):
         """check that political party details are valid"""
         if len(name.strip()) == 0:
-            return Responses.bad_request("Party Name cannot be empty"),400
+            return Responses.bad_request("Party Name cannot be empty"), 400
         if len(hqAddress.strip()) == 0:
             return Responses.bad_request("hqAddress name cannot be empty"), 400
         else:
             if len(logoUrl.strip()) == 0:
                 return Responses.bad_request("LogoUrl cannot be empty"), 400
 
+    @staticmethod
+    def validate_integers(input):
+        if isinstance(input, str):
+            return Responses.bad_request("Only Strings are allowed for this method"), 400
+    @staticmethod
+    def validate_strings(input):
+        if isinstance(input, str):
+            return Responses.bad_request("Only Strings are allowed for this method"), 400
